6d03d86fa9d58398971ac181bad45e4cce04227e
Vishal-m <vishalmahesh.study@gmail.com>
2024-03-07 01:34:18 +0530

    fix ui: hero

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,67 +1,88 @@
-import { brainwave } from "../assets/index.js";
-import { navigation } from "../constants/index.js";
 import { useLocation } from "react-router-dom";
-import MenuSvg from "../assets/svg/MenuSvg.jsx";
-import { HamburgerMenu } from "./design/Header.jsx";
-import Button from "./button.jsx";
-import { useState } from "react";
-const Header = () => {
-  const [openNaviagtion, setOpenNavigation] = useState(false);
-  const pathname = useLocation();
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
-  const handleOpen = () => {
-    setOpenNavigation((prev) => !prev);
+import { brainwave } from "../assets";
+import { navigation } from "../constants";
+import Button from "./button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
+
+const Header = () => {
+  const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
   };
+
+  const handleClick = () => {
+    if (!openNavigation) return;
+
+    enablePageScroll();
+    setOpenNavigation(false);
+  };
+
   return (
     <div
-      className="fixed w-full top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b
-    border-n-6 "
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
     >
-      <div className="flex items-center gap-2 px-5 lg:px-7.5 xl:px-10 max-lg-py-10">
-        <a className="block w-[12rem] py-6 lg:py-0 z-10" href="#hero">
-          <img src={brainwave} alt="logo" height={120} width={150} />
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
 
         <nav
           className={`${
-            openNaviagtion ? "flex" : "hidden"
-          } bg-n-8 lg:static lg:flex w-full lg:mx-auto lg:bg-transparent fixed top-0 left-0 right-0 bottom-0`}
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
-          <div className="relative z-2 flex mt-28 lg:mt-0  flex-col items-center justify-center m-auto lg:flex-row ">
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
-                href={item.url}
                 key={item.id}
-                onClick={handleOpen}
-                className={`block relative cursor-pointer text-2xl lg:text-xs uppercase transition-colors  hover:text-n-1 ${
+                href={item.url}
+                onClick={handleClick}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:font-semibold ${
-                  pathname.hash === item.url ? "text-n-1" : "text-n-4"
-                } lg:leading-5`}
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
                 {item.title}
               </a>
             ))}
           </div>
 
-          <div className="flex items-center">
-            <a
-              href="#signin"
-              className="hidden lg:block text-n-1/50  text-xs leading-5 lg:font-semibold transition-colors hover:text-n-1 mr-3"
-            >
-              New account
-            </a>
-            <Button className="hidden lg:flex">Sign in</Button>
-          </div>
           <HamburgerMenu />
         </nav>
 
-        <Button
-          className={"ml-auto lg:hidden"}
-          px={"px-3"}
-          onClick={handleOpen}
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
-          <MenuSvg openNavigation={openNaviagtion} />
+          New account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign in
+        </Button>
+
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
     </div>
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,17 +1,23 @@
 import { curve, heroBackground, robot } from "../assets";
 import Button from "./button";
 import Section from "./section";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
     <Section
       className={`pt-[12rem] -mt-[5.25rem]`}
-      crosess
+      crosess      
       crossesOffset={"lg:translate-y-[5.35rem]"}
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of AI Chatting with {""}
@@ -39,19 +45,36 @@ const Hero = () => {
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.65rem]" />
               <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
-                <img src={robot} alt="hero" className="w-full scale-[1.7] -translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-[22%]" height={490} width={1024} />
+                <img
+                  src={robot}
+                  alt="AI"
+                  className="w-full scale-[1.7] -translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-[22%]"
+                  height={490}
+                  width={1024}
+                />
               </div>
+              <ScrollParallax isAbsolutelyPositioned zIndex={1}>
+                <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] bg-n-9/40 px-1 py-3 backdrop-blur border border-n-1/10 rounded-xl xl:flex justify-center items-center">
+                  {Array(4)
+                    .fill(null)
+                    .map((_, index) => (
+                      <li className="mx-4">{index}</li>
+                    ))}
+                </ul>
+              </ScrollParallax>
             </div>
+            <Gradient />
           </div>
           <div className="absolute -top-[60%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
               src={heroBackground}
               alt="hero"
-              className="w-full"
+              className="w-full overflow-hidden"
               width={1440}
               height={1800}
             />
           </div>
+          <BackgroundCircles parallaxRef={parallaxRef} />
         </div>
       </div>
     </Section>
